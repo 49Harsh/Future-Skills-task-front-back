9a0ddabc44e257debbf4d6fe94d8d7bd4cccb900
Harsh-Portfolio <49harshyadav@gmail.com>
2024-08-29 11:19:50 +0530

    matching ui as given in image

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,6 @@ app.get('/ping', (req, res) => {
 const cardRoutes = require('./routes/cardRoutes');
 app.use('/cards', cardRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
